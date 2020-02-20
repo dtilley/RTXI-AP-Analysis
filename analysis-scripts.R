76bf17b7a12b39e4994c70c40bc0843e7825a098
dtilley <drew.tilley@gmail.com>
2020-02-20 14:41:41 -0500

    added interpolation

--- analysis-scripts.R
+++ analysis-scripts.R
@@ -40,15 +40,15 @@ apVoltages <- function(x,ndxDF,dt=0.1){
 }
 
 
-apDurations <- function(x,apVoltages,dt=0.1,preThreshold.t=20){
-    # x: assumed to be a single dimensional vector of voltage #
-    # dt: period between samples and defaults to 0.1 ms #
-    # apVoltages: a dataframe indices from apVoltages  #
-    # preThreshold.t: calculates the pre-threshod voltage at time(t) before the peak voltage #
-    # preThreshold.divisions: uses the preTheshold times at divisions for durations.
+apDurations <- function(t,mV,apVoltages,preThreshold.dt=20){
+    ## t: time vector 
+    ## mV: membrane voltage vector
+    ## preThreshold.dt: the time prior to the peak voltage to calculate diastolic membrane voltage 
+    ## apVoltages: a dataframe of indices from apVoltages  
 
     num.of.APs <- nrow(apVoltages)
-    preTheshold.V <- rep(NA,num.of.APs)
+    preThreshold.V <- rep(NA,num.of.APs)
+    
     first.ndx <- rep(NA,num.of.APs)
     last.ndx <- rep(NA,num.of.APs)
     amp <- rep(NA,num.of.APs)
@@ -63,25 +63,25 @@ apDurations <- function(x,apVoltages,dt=0.1,preThreshold.t=20){
     ap80 <- rep(NA,num.of.APs)
     ap90 <- rep(NA,num.of.APs)
 
-    apd10.firstndx <- rep(NA,num.of.APs)
-    apd20.firstndx <- rep(NA,num.of.APs)
-    apd30.firstndx <- rep(NA,num.of.APs)
-    apd40.firstndx <- rep(NA,num.of.APs)
-    apd50.firstndx <- rep(NA,num.of.APs)
-    apd60.firstndx <- rep(NA,num.of.APs)
-    apd70.firstndx <- rep(NA,num.of.APs)
-    apd80.firstndx <- rep(NA,num.of.APs)
-    apd90.firstndx <- rep(NA,num.of.APs)
+    apd10.t1 <- rep(NA,num.of.APs)
+    apd20.t1 <- rep(NA,num.of.APs)
+    apd30.t1 <- rep(NA,num.of.APs)
+    apd40.t1 <- rep(NA,num.of.APs)
+    apd50.t1 <- rep(NA,num.of.APs)
+    apd60.t1 <- rep(NA,num.of.APs)
+    apd70.t1 <- rep(NA,num.of.APs)
+    apd80.t1 <- rep(NA,num.of.APs)
+    apd90.t1 <- rep(NA,num.of.APs)
 
-    apd10.lastndx <- rep(NA,num.of.APs)
-    apd20.lastndx <- rep(NA,num.of.APs)
-    apd30.lastndx <- rep(NA,num.of.APs)
-    apd40.lastndx <- rep(NA,num.of.APs)
-    apd50.lastndx <- rep(NA,num.of.APs)
-    apd60.lastndx <- rep(NA,num.of.APs)
-    apd70.lastndx <- rep(NA,num.of.APs)
-    apd80.lastndx <- rep(NA,num.of.APs)
-    apd90.lastndx <- rep(NA,num.of.APs)
+    apd10.t2 <- rep(NA,num.of.APs)
+    apd20.t2 <- rep(NA,num.of.APs)
+    apd30.t2 <- rep(NA,num.of.APs)
+    apd40.t2 <- rep(NA,num.of.APs)
+    apd50.t2 <- rep(NA,num.of.APs)
+    apd60.t2 <- rep(NA,num.of.APs)
+    apd70.t2 <- rep(NA,num.of.APs)
+    apd80.t2 <- rep(NA,num.of.APs)
+    apd90.t2 <- rep(NA,num.of.APs)
 
     apd10 <- rep(NA,num.of.APs)
     apd20 <- rep(NA,num.of.APs)    
@@ -93,144 +93,226 @@ apDurations <- function(x,apVoltages,dt=0.1,preThreshold.t=20){
     apd80 <- rep(NA,num.of.APs)    
     apd90 <- rep(NA,num.of.APs)
     
-    # Calculates preThreshold.divisions #
-    preThreshold.ndx <- apVoltages$peakV.ndx-as.integer(preThreshold.t/dt)
-    if (length(which(preThreshold.ndx<0))==0){
-        first.ndx <- preThreshold.ndx
-        last.ndx[1:(length(last.ndx)-1)] <- first.ndx[2:length(first.ndx)]
-        last.ndx[length(last.ndx)] <- (2*first.ndx[length(first.ndx)]-first.ndx[(length(first.ndx)-1)])
-        if (last.ndx[length(last.ndx)]>length(x)) {
-            last.ndx[length(last.ndx)] <- length(x)
-        }
-    } else if(length(which(preThreshold.ndx<0))==1 && which(preThreshold.ndx<0)==1) {
-        preThreshold.ndx[1] <- 1
-        first.ndx <- preThreshold.ndx
-        last.ndx[1:(length(last.ndx)-1)] <- first.ndx[2:length(first.ndx)]
-        last.ndx[length(last.ndx)] <- (2*first.ndx[length(first.ndx)]-first.ndx[(length(first.ndx)-1)])
-        if (last.ndx[length(last.ndx)]>length(x)) {
-            last.ndx[length(last.ndx)] <- length(x)
-        }
-    } else {
-        print("Check ndxDF. The preTheshold.t indices are not within the bounds.")
+    ## Calculates first.ndx
+    time.firstndx <- t[apVoltages$peakV.ndx]-preThreshold.dt
+    for (i in 1:length(time.firstndx)){
+        first.ndx[i] <- which.min(abs(t-time.firstndx[i]))
+    }
+    ## Calculates last.ndx
+    last.ndx[1:(length(last.ndx)-1)] <- first.ndx[2:length(first.ndx)]
+    last.ndx[length(last.ndx)] <- as.integer(mean(last.ndx[1:(length(last.ndx)-1)]-first.ndx[1:(length(last.ndx)-1)]))+first.ndx[length(first.ndx)]
+
+    ## Check if first.ndx and last.ndx exsist
+    if (length(which(first.ndx<=1))!=0 || length(which(first.ndx>=length(t)))!=0) {
+        print("First indices are out of range.")
+        return()
+    } else if ((length(which(last.ndx<=1))!=0 || length(which(last.ndx>=length(t)))!=0)) {
+        print("Last indices are out of range.")
         return()
     }
-        # Calculates the preTheshold.V    
-        # Calculates APDs
+
+    ## Calculates APDs
     for (i in 1:num.of.APs){
-        preTheshold.V[i] <- mean(x[first.ndx[i]:(first.ndx[i]+as.integer(5/dt))]) # Mean V
-        amp[i] <- apVoltages$peakV[i]-preTheshold.V[i]
-        ap10[i] <- preTheshold.V[i]+0.9*amp[i]
-        ap20[i] <- preTheshold.V[i]+0.8*amp[i]
-        ap30[i] <- preTheshold.V[i]+0.7*amp[i]
-        ap40[i] <- preTheshold.V[i]+0.6*amp[i]
-        ap50[i] <- preTheshold.V[i]+0.5*amp[i]
-        ap60[i] <- preTheshold.V[i]+0.4*amp[i]
-        ap70[i] <- preTheshold.V[i]+0.3*amp[i]
-        ap80[i] <- preTheshold.V[i]+0.2*amp[i]
-        ap90[i] <- preTheshold.V[i]+0.1*amp[i]
-
-        apd10.firstndx[i] <- which.min(abs(x[first.ndx[i]:apVoltages$peakV.ndx[i]]-ap10[i]))+first.ndx[i]
-        apd20.firstndx[i] <- which.min(abs(x[first.ndx[i]:apVoltages$peakV.ndx[i]]-ap20[i]))+first.ndx[i]
-        apd30.firstndx[i] <- which.min(abs(x[first.ndx[i]:apVoltages$peakV.ndx[i]]-ap30[i]))+first.ndx[i]
-        apd40.firstndx[i] <- which.min(abs(x[first.ndx[i]:apVoltages$peakV.ndx[i]]-ap40[i]))+first.ndx[i]
-        apd50.firstndx[i] <- which.min(abs(x[first.ndx[i]:apVoltages$peakV.ndx[i]]-ap50[i]))+first.ndx[i]
-        apd60.firstndx[i] <- which.min(abs(x[first.ndx[i]:apVoltages$peakV.ndx[i]]-ap60[i]))+first.ndx[i]
-        apd70.firstndx[i] <- which.min(abs(x[first.ndx[i]:apVoltages$peakV.ndx[i]]-ap70[i]))+first.ndx[i]
-        apd80.firstndx[i] <- which.min(abs(x[first.ndx[i]:apVoltages$peakV.ndx[i]]-ap80[i]))+first.ndx[i]
-        apd90.firstndx[i] <- which.min(abs(x[first.ndx[i]:apVoltages$peakV.ndx[i]]-ap90[i]))+first.ndx[i]
-
-        apd10.lastndx[i] <- which.min(abs(x[apVoltages$peakV.ndx[i]:last.ndx[i]]-ap10[i]))+apVoltages$peakV.ndx[i]
-        apd20.lastndx[i] <- which.min(abs(x[apVoltages$peakV.ndx[i]:last.ndx[i]]-ap20[i]))+apVoltages$peakV.ndx[i]
-        apd30.lastndx[i] <- which.min(abs(x[apVoltages$peakV.ndx[i]:last.ndx[i]]-ap30[i]))+apVoltages$peakV.ndx[i]
-        apd40.lastndx[i] <- which.min(abs(x[apVoltages$peakV.ndx[i]:last.ndx[i]]-ap40[i]))+apVoltages$peakV.ndx[i]
-        apd50.lastndx[i] <- which.min(abs(x[apVoltages$peakV.ndx[i]:last.ndx[i]]-ap50[i]))+apVoltages$peakV.ndx[i]
-        apd60.lastndx[i] <- which.min(abs(x[apVoltages$peakV.ndx[i]:last.ndx[i]]-ap60[i]))+apVoltages$peakV.ndx[i]
-        apd70.lastndx[i] <- which.min(abs(x[apVoltages$peakV.ndx[i]:last.ndx[i]]-ap70[i]))+apVoltages$peakV.ndx[i]
-        apd80.lastndx[i] <- which.min(abs(x[apVoltages$peakV.ndx[i]:last.ndx[i]]-ap80[i]))+apVoltages$peakV.ndx[i]
-        apd90.lastndx[i] <- which.min(abs(x[apVoltages$peakV.ndx[i]:last.ndx[i]]-ap90[i]))+apVoltages$peakV.ndx[i]
-
-        apd10[i] <- (apd10.lastndx[i] - apd10.firstndx[i])*dt
-        apd20[i] <- (apd20.lastndx[i] - apd20.firstndx[i])*dt
-        apd30[i] <- (apd30.lastndx[i] - apd30.firstndx[i])*dt
-        apd40[i] <- (apd40.lastndx[i] - apd40.firstndx[i])*dt
-        apd50[i] <- (apd50.lastndx[i] - apd50.firstndx[i])*dt
-        apd60[i] <- (apd60.lastndx[i] - apd60.firstndx[i])*dt
-        apd70[i] <- (apd70.lastndx[i] - apd70.firstndx[i])*dt
-        apd80[i] <- (apd80.lastndx[i] - apd80.firstndx[i])*dt
-        apd90[i] <- (apd90.lastndx[i] - apd90.firstndx[i])*dt
+        ## Calculates preThreshold.V over 20 indices forward from first.ndx        
+        preThreshold.V[i] <- mean(mV[first.ndx[i]:(first.ndx[i]+20)])
+        if (var(mV[first.ndx[i]:(first.ndx[i]+20)])>0.01*preThreshold.V[i]){
+            print("Warning: High variance in preThreshold.V.")
+        }
+        amp[i] <- apVoltages$peakV[i]-preThreshold.V[i]
+        ap10[i] <- preThreshold.V[i]+0.9*amp[i]
+        ap20[i] <- preThreshold.V[i]+0.8*amp[i]
+        ap30[i] <- preThreshold.V[i]+0.7*amp[i]
+        ap40[i] <- preThreshold.V[i]+0.6*amp[i]
+        ap50[i] <- preThreshold.V[i]+0.5*amp[i]
+        ap60[i] <- preThreshold.V[i]+0.4*amp[i]
+        ap70[i] <- preThreshold.V[i]+0.3*amp[i]
+        ap80[i] <- preThreshold.V[i]+0.2*amp[i]
+        ap90[i] <- preThreshold.V[i]+0.1*amp[i]
         
+        ## Calculate apd10
+        ## pre-peak points to interpolate
+        p1.ndx <- min(which(mV[first.ndx[i]:apVoltages$peakV.ndx[i]]>=ap10[i]))+first.ndx[i]-1
+        p2.ndx <- p1.ndx-1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t1 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap10[i])
+        apd10.t1[i] <- t1$y
+        ## post-peak points to interpolate
+        p1.ndx <- max(which(mV[apVoltages$peakV.ndx[i]:last.ndx[i]]>=ap10[i]))+apVoltages$peakV.ndx[i]-1
+        p2.ndx <- p1.ndx+1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t2 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap10[i])
+        apd10.t2[i] <- t2$y
+        apd10[i] <- apd10.t2[i]-apd10.t1[i]
+
+        ## Calculate apd20
+        ## pre-peak points to interpolate
+        p1.ndx <- min(which(mV[first.ndx[i]:apVoltages$peakV.ndx[i]]>=ap20[i]))+first.ndx[i]-1
+        p2.ndx <- p1.ndx-1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t1 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap20[i])
+        apd20.t1[i] <- t1$y
+        ## post-peak points to interpolate
+        p1.ndx <- max(which(mV[apVoltages$peakV.ndx[i]:last.ndx[i]]>=ap20[i]))+apVoltages$peakV.ndx[i]-1
+        p2.ndx <- p1.ndx+1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t2 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap20[i])
+        apd20.t2[i] <- t2$y
+        apd20[i] <- apd20.t2[i]-apd20.t1[i]
+
+        ## Calculate apd30
+        ## pre-peak points to interpolate
+        p1.ndx <- min(which(mV[first.ndx[i]:apVoltages$peakV.ndx[i]]>=ap30[i]))+first.ndx[i]-1
+        p2.ndx <- p1.ndx-1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t1 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap30[i])
+        apd30.t1[i] <- t1$y
+        ## post-peak points to interpolate
+        p1.ndx <- max(which(mV[apVoltages$peakV.ndx[i]:last.ndx[i]]>=ap30[i]))+apVoltages$peakV.ndx[i]-1
+        p2.ndx <- p1.ndx+1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t2 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap30[i])
+        apd30.t2[i] <- t2$y
+        apd30[i] <- apd30.t2[i]-apd30.t1[i]
+
+        ## Calculate apd40
+        ## pre-peak points to interpolate
+        p1.ndx <- min(which(mV[first.ndx[i]:apVoltages$peakV.ndx[i]]>=ap40[i]))+first.ndx[i]-1
+        p2.ndx <- p1.ndx-1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t1 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap40[i])
+        apd40.t1[i] <- t1$y
+        ## post-peak points to interpolate
+        p1.ndx <- max(which(mV[apVoltages$peakV.ndx[i]:last.ndx[i]]>=ap40[i]))+apVoltages$peakV.ndx[i]-1
+        p2.ndx <- p1.ndx+1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t2 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap40[i])
+        apd40.t2[i] <- t2$y
+        apd40[i] <- apd40.t2[i]-apd40.t1[i]
+
+        ## Calculate apd50
+        ## pre-peak points to interpolate
+        p1.ndx <- min(which(mV[first.ndx[i]:apVoltages$peakV.ndx[i]]>=ap50[i]))+first.ndx[i]-1
+        p2.ndx <- p1.ndx-1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t1 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap50[i])
+        apd50.t1[i] <- t1$y
+        ## post-peak points to interpolate
+        p1.ndx <- max(which(mV[apVoltages$peakV.ndx[i]:last.ndx[i]]>=ap50[i]))+apVoltages$peakV.ndx[i]-1
+        p2.ndx <- p1.ndx+1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t2 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap50[i])
+        apd50.t2[i] <- t2$y
+        apd50[i] <- apd50.t2[i]-apd50.t1[i]
+
+        ## Calculate apd60
+        ## pre-peak points to interpolate
+        p1.ndx <- min(which(mV[first.ndx[i]:apVoltages$peakV.ndx[i]]>=ap60[i]))+first.ndx[i]-1
+        p2.ndx <- p1.ndx-1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t1 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap60[i])
+        apd60.t1[i] <- t1$y
+        ## post-peak points to interpolate
+        p1.ndx <- max(which(mV[apVoltages$peakV.ndx[i]:last.ndx[i]]>=ap60[i]))+apVoltages$peakV.ndx[i]-1
+        p2.ndx <- p1.ndx+1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t2 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap60[i])
+        apd60.t2[i] <- t2$y
+        apd60[i] <- apd60.t2[i]-apd60.t1[i]
+
+        ## Calculate apd70
+        ## pre-peak points to interpolate
+        p1.ndx <- min(which(mV[first.ndx[i]:apVoltages$peakV.ndx[i]]>=ap70[i]))+first.ndx[i]-1
+        p2.ndx <- p1.ndx-1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t1 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap70[i])
+        apd70.t1[i] <- t1$y
+        ## post-peak points to interpolate
+        p1.ndx <- max(which(mV[apVoltages$peakV.ndx[i]:last.ndx[i]]>=ap70[i]))+apVoltages$peakV.ndx[i]-1
+        p2.ndx <- p1.ndx+1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t2 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap70[i])
+        apd70.t2[i] <- t2$y
+        apd70[i] <- apd70.t2[i]-apd70.t1[i]
+
+        ## Calculate apd80
+        ## pre-peak points to interpolate
+        p1.ndx <- min(which(mV[first.ndx[i]:apVoltages$peakV.ndx[i]]>=ap80[i]))+first.ndx[i]-1
+        p2.ndx <- p1.ndx-1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t1 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap80[i])
+        apd80.t1[i] <- t1$y
+        ## post-peak points to interpolate
+        p1.ndx <- max(which(mV[apVoltages$peakV.ndx[i]:last.ndx[i]]>=ap80[i]))+apVoltages$peakV.ndx[i]-1
+        p2.ndx <- p1.ndx+1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t2 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap80[i])
+        apd80.t2[i] <- t2$y
+        apd80[i] <- apd80.t2[i]-apd80.t1[i]
+
+        ## Calculate apd90
+        ## pre-peak points to interpolate
+        p1.ndx <- min(which(mV[first.ndx[i]:apVoltages$peakV.ndx[i]]>=ap90[i]))+first.ndx[i]-1
+        p2.ndx <- p1.ndx-1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t1 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap90[i])
+        apd90.t1[i] <- t1$y
+        ## post-peak points to interpolate
+        p1.ndx <- max(which(mV[apVoltages$peakV.ndx[i]:last.ndx[i]]>=ap90[i]))+apVoltages$peakV.ndx[i]-1
+        p2.ndx <- p1.ndx+1
+        
+        t.interpolate <- t[c(p1.ndx,p2.ndx)]
+        mV.interpolate <- mV[c(p1.ndx,p2.ndx)]
+        t2 <- approx(x=mV.interpolate,y=t.interpolate,xout=ap90[i])
+        apd90.t2[i] <- t2$y
+        apd90[i] <- apd90.t2[i]-apd90.t1[i]
+                
     }
 
-    rtndf <- as.data.frame(cbind(preTheshold.V,amp,first.ndx,last.ndx,apd10,apd20,apd30,apd40,apd50,apd60,apd70,apd80,apd90,
+    rtndf <- as.data.frame(cbind(preThreshold.V,amp,first.ndx,last.ndx,apd10,apd20,apd30,apd40,apd50,apd60,apd70,apd80,apd90,
                                  ap10,ap20,ap30,ap40,ap50,ap60,ap70,ap80,ap90,apd10,apd20,apd30,apd40,
                                  apd50,apd60,apd70,apd80,apd90,
-                                 apd10.firstndx,apd20.firstndx,apd30.firstndx,
-                                 apd40.firstndx,apd50.firstndx,apd60.firstndx,
-                                 apd70.firstndx,apd80.firstndx,apd90.firstndx,
-                                 apd10.lastndx,apd20.lastndx,apd30.lastndx,
-                                 apd40.lastndx,apd50.lastndx,apd60.lastndx,
-                                 apd70.lastndx,apd80.lastndx,apd90.lastndx))
+                                 apd10.t1,apd20.t1,apd30.t1,
+                                 apd40.t1,apd50.t1,apd60.t1,
+                                 apd70.t1,apd80.t1,apd90.t1,
+                                 apd10.t2,apd20.t2,apd30.t2,
+                                 apd40.t2,apd50.t2,apd60.t2,
+                                 apd70.t2,apd80.t2,apd90.t2))
     return(rtndf)
     
-}
-
-
-
-apfind.variantDT <- function(t.ms,y,minlength=2){
-    # minlength defines the minimum length of AP signal above the mean potential: default is 2 ms
-    # AP peaks
-    if (length(y)!=length(t.ms)){
-        print("Voltage and Time vectors do not have the same length.")
-        return
-    }
-    signalNDX <- which(y>mean(y))
-    apNUM <- NULL
-    first.ndx <- NULL
-    last.ndx <- NULL
-    
-    if (length(signalNDX)==0){
-        print("No APs found. Check input.")
-        return
-    }
-
-    tmp <- 1
-    apCount <- 1
-    apNUM[apCount] <- apCount
-    for(i in 2:length(signalNDX)){
-        if (signalNDX[i-1]==signalNDX[i]-1) {
-            # continuous
-            if (tmp==1){
-                first.ndx[apCount] <- signalNDX[i-1]
-                tmp <- tmp+1
-            } else if (i==length(signalNDX)){
-                # AP cut off
-                last.ndx[apCount] <- signalNDX[i]
-            } else {
-                tmp <- tmp+1
-            }
-            
-
-         } else {
-             # discontinuous
-             tmp <- 1
-             if (t.ms[signalNDX[i-1]]-t.ms[first.ndx[apCount]]>=minlength){
-                 # previous AP meets the minimum length threshold for AP
-                 last.ndx[apCount] <- signalNDX[i-1]
-                 apCount <- apCount+1
-                 apNUM[apCount] <- apCount
-             } else {
-                 # previous AP shorter than minimum length
-                 first.ndx[apCount] <- signalNDX[i]
-             }
-         } 
-    }
-    # Check identified APs
-    if (length(apNUM)==length(first.ndx) && length(apNUM)==length(last.ndx)){
-        retdf <- as.data.frame(cbind(apNUM,first.ndx,last.ndx))
-        return(retdf)
-    } else {
-        print("Nope.")
-    }
 }
 
 # Generic plot fucntion with figure publication standards from the JGP
--- apVisual.R
+++ apVisual.R
@@ -8,22 +8,22 @@ jgpplotter <- function(x,y,mar=c(5.1,5.1,5.1,2.1),lwdpt=0.75,...){
 }
 
 
-visualizeAPDs <- function(t,Vm,apVoltages,apDurations,...){
-    jgpplotter(t,Vm,type="l",...)
+visualizeAPDs <- function(t,mV,apVoltages,apDurations,...){
+    jgpplotter(t,mV,type="l",...)
     abline(v=t[apVoltages$peakV.ndx],col="blue")
     abline(v=t[apDurations$first.ndx],lty=1,col="seagreen")
-    abline(v=t[apDurations$last.ndx],lty=2,col="seagreen")
+    abline(v=t[apDurations$last.ndx],lty=2,col="magenta")
     colors <- rainbow(n=9)
     for (i in 1:nrow(apDurations)){
-        lines(t[apDurations$apd90.firstndx[i]:apDurations$apd90.lastndx[i]],y=rep(apDurations$ap90[i],length(t[apDurations$apd90.firstndx[i]:apDurations$apd90.lastndx[i]])),col=colors[1])
-        lines(t[apDurations$apd80.firstndx[i]:apDurations$apd80.lastndx[i]],y=rep(apDurations$ap80[i],length(t[apDurations$apd80.firstndx[i]:apDurations$apd80.lastndx[i]])),col=colors[2])
-        lines(t[apDurations$apd70.firstndx[i]:apDurations$apd70.lastndx[i]],y=rep(apDurations$ap70[i],length(t[apDurations$apd70.firstndx[i]:apDurations$apd70.lastndx[i]])),col=colors[3])
-        lines(t[apDurations$apd60.firstndx[i]:apDurations$apd60.lastndx[i]],y=rep(apDurations$ap60[i],length(t[apDurations$apd60.firstndx[i]:apDurations$apd60.lastndx[i]])),col=colors[4])
-        lines(t[apDurations$apd50.firstndx[i]:apDurations$apd50.lastndx[i]],y=rep(apDurations$ap50[i],length(t[apDurations$apd50.firstndx[i]:apDurations$apd50.lastndx[i]])),col=colors[5])
-        lines(t[apDurations$apd40.firstndx[i]:apDurations$apd40.lastndx[i]],y=rep(apDurations$ap40[i],length(t[apDurations$apd40.firstndx[i]:apDurations$apd40.lastndx[i]])),col=colors[6])
-        lines(t[apDurations$apd30.firstndx[i]:apDurations$apd30.lastndx[i]],y=rep(apDurations$ap30[i],length(t[apDurations$apd30.firstndx[i]:apDurations$apd30.lastndx[i]])),col=colors[7])
-        lines(t[apDurations$apd20.firstndx[i]:apDurations$apd20.lastndx[i]],y=rep(apDurations$ap20[i],length(t[apDurations$apd20.firstndx[i]:apDurations$apd20.lastndx[i]])),col=colors[8])
-        lines(t[apDurations$apd10.firstndx[i]:apDurations$apd10.lastndx[i]],y=rep(apDurations$ap10[i],length(t[apDurations$apd10.firstndx[i]:apDurations$apd10.lastndx[i]])),col=colors[9])
+        segments(x0=apDurations$apd10.t1[i],y0=apDurations$ap10[i],x1=apDurations$apd10.t2[i],y1=apDurations$ap10[i],col=colors[1])
+        segments(x0=apDurations$apd20.t1[i],y0=apDurations$ap20[i],x1=apDurations$apd20.t2[i],y1=apDurations$ap20[i],col=colors[2])
+        segments(x0=apDurations$apd30.t1[i],y0=apDurations$ap30[i],x1=apDurations$apd30.t2[i],y1=apDurations$ap30[i],col=colors[3])
+        segments(x0=apDurations$apd40.t1[i],y0=apDurations$ap40[i],x1=apDurations$apd40.t2[i],y1=apDurations$ap40[i],col=colors[4])
+        segments(x0=apDurations$apd50.t1[i],y0=apDurations$ap50[i],x1=apDurations$apd50.t2[i],y1=apDurations$ap50[i],col=colors[5])
+        segments(x0=apDurations$apd60.t1[i],y0=apDurations$ap60[i],x1=apDurations$apd60.t2[i],y1=apDurations$ap60[i],col=colors[6])
+        segments(x0=apDurations$apd70.t1[i],y0=apDurations$ap70[i],x1=apDurations$apd70.t2[i],y1=apDurations$ap70[i],col=colors[7])
+        segments(x0=apDurations$apd80.t1[i],y0=apDurations$ap80[i],x1=apDurations$apd80.t2[i],y1=apDurations$ap80[i],col=colors[8])
+        segments(x0=apDurations$apd90.t1[i],y0=apDurations$ap90[i],x1=apDurations$apd90.t2[i],y1=apDurations$ap90[i],col=colors[9])
     }
 
 }
